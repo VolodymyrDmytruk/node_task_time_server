4e16977fd87112f88b95b46087b414db9af3548c
olha-hoshko <shengkeru@gmail.com>
2023-07-17 15:45:04 +0300

    Completed the fourth and the fifth issues

--- server.js
+++ server.js
@@ -3,18 +3,46 @@ import minimist from 'minimist';
 const argv = minimist(process.argv.slice(2));
 
 const app = express();
+const port = 3000;
+
+const interval = argv.i || 500; // default 0.5 seconds
+const timeout = argv.t || 2500; // default 2.5 seconds
+
+const addZero = (timeNumber) => {
+  return timeNumber > 9 ? timeNumber : `0${timeNumber}`;
+}
+
+const transformDate = (currentTime) => {
+  const year = currentTime.getUTCFullYear();
+  const month = addZero(currentTime.getUTCMonth() + 1);
+  const day = addZero(currentTime.getUTCDate());
+  const hours = addZero(currentTime.getUTCHours());
+  const minutes = addZero(currentTime.getUTCMinutes());
+  const seconds = addZero(currentTime.getUTCSeconds());
+
+  return `${day}.${month}.${year}, ${hours}:${minutes}:${seconds}`;
+}
 
 app.get('/', (req, res) => {
-	console.log('Start');
-	res.send('some text');
+  console.log('Start');
+
+  const intervalTimer = setInterval(() => {
+    const currentTime = new Date();
+    console.log(transformDate(currentTime));
+  }, interval);
+
+  setTimeout(() => {
+    clearInterval(intervalTimer);
+
+    const currentTime = new Date();
+    const timeUTC = transformDate(currentTime);
+    console.log(timeUTC);
+
+    console.log('Stop');
+    res.send(timeUTC);
+  }, timeout);
 })
 
-app.listen(3000);
-
-
-const interval = argv.interval || 500; // за замовчуванням 500 мс (0.5 секунди)
-const timeout = argv.timeout || 2500; // за замовчуванням 2500 мс (2.5 секунд)
-
-console.log('Interval:', interval);
-console.log('Timeout:', timeout);
-let now = new Date();
+app.listen(port, () => {
+  console.log('Server is listening on', port);
+});
